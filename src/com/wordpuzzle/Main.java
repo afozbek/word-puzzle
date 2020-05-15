63055c27b79443199a7e442c54209d9a86152ea0
afozbek <furkanozbek1995@gmail.com>
2020-05-15 13:24:12 +0300

    initial commit

---
+++ src/com/wordpuzzle/Main.java
@@ -0,0 +1,13 @@
+package com.wordpuzzle;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+	// write your code here
+        System.out.println("Hello World");
+    }
+}
